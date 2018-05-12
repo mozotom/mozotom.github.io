1de2fa1f8a2609a614ea106ebf7c7112119c1b45
mozotom <mozotom@users.noreply.github.com>
2018-05-12 11:29:17 -0500

    init

---
+++ projects/stock-market/stock-market.js
@@ -0,0 +1,21 @@
+function _(id) {
+  return document.getElementById(id);
+}
+
+function getText(url) {
+    var request = new XMLHttpRequest();
+    request.open('GET', url, true);
+    request.send(null);
+    request.onreadystatechange = function () {
+        if (request.readyState === 4 && request.status === 200) {
+            var type = request.getResponseHeader('Content-Type');
+            if (type.indexOf("text") !== 1) {
+                return request.responseText;
+            }
+        }
+    }
+}
+
+function init() {
+  _("result").innerText = getText("sp500.csv");
+}
